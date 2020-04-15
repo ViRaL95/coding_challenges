62a29e4fda4a795987e69cd2f041dc6081ad4a3b
Varun Rao <vrao@thedatasherpas.com>
2020-04-15 17:13:37 -0400

    coding challenges

---
+++ find_closest_value_in_bst.py
@@ -0,0 +1,11 @@
+"""
+Given a BST find the closest value to it
+
+Number = 3
+
+          6
+       0    12
+    -3    4    19
+        2   5
+
+"""
---
+++ find_good_subarray.py
@@ -0,0 +1,16 @@
+""""
+
+B = 4
+ [1, 2, 3, 4, 5]
+
+{1: 0, 3: 1, 6: 2, 10: 3, 15:4}
+B=6
+
+[-4,-7,5,3,8,9]
+
+    4
+  11  6
+
+
+Where the difference in sum <` B. Sum of all elements of that subarray is less than B
+"""
